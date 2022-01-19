769bb337c3911be3e33aab489cd3ddb14795aaaa
chipunderblade <pfjhyyj@qq.com>
2022-01-19 16:13:43 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import mpa from "@chipunderblade/vite-plugin-mpa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    mpa(),
+    react(),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "src")
+      }
+    ]
+  }
+});
